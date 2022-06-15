57129b7dfc9be3e6d7579d90af7d0c1581648b84
janenool <janenool@gmail.com>
2022-06-15 18:03:25 +0300

    landingPage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,12 @@ import { ListEmployeesComponent } from './employees/list-employees.component';
 import { CreateEmployeeComponent } from './employees/create-employee.component';
 import { FormsModule } from '@angular/forms';
 import { RouterModule, Routes } from '@angular/router';
-//import {bsDatepickerModule} from 'ngx-bootstrap/datepicker';
+import { LandingPageComponent } from './employees/landing-page/landing-page.component';
 
 const appRoutes: Routes = [
   {path: 'list', component: ListEmployeesComponent},
   {path: 'create', component: CreateEmployeeComponent},
+  {path: 'landing-page', component: LandingPageComponent},
   {path: '', redirectTo:'/list', pathMatch:'full'}];
 
 @NgModule({
@@ -19,6 +20,7 @@ const appRoutes: Routes = [
     AppComponent,
     ListEmployeesComponent,
     CreateEmployeeComponent,
+    LandingPageComponent,
     
   ],
   imports: [
